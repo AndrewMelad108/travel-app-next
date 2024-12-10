6725965bd7c67c298aea90428a44d5c3c322876c
Andrew Melad <andrewmelad108@gmail.com>
2024-12-10 14:12:02 +0200

    add env in next

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output:'standalone'
+    output:'standalone',
+    env: {
+        URL_NEXT_API: 'https://travel-app-next-delta.vercel.app',
+      },
 };
 
 export default nextConfig;
